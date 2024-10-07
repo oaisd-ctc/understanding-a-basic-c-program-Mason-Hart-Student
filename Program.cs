9fbd5a418f07d76fa3f4a8c3102b89ef4fa6eca6
Mason-Hart-Student <maha6a5@student.oaisd.org>
2024-10-07 12:59:48 +0000

    Learned basics of C#

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("My name is Mason Hart");
+
+        //Using Variables
+        int age = 16;
+        string greeting = "Hello World";
+        string firstname = "Mason";
+        string lastname = "Hart";
+        double money = 2.79;
+
+        Console.WriteLine(greeting + "! " + "My name is " + firstname + " " + lastname + ". I am " + age + " years old. I just spent $" + money + " on an Energy Drink.");
+    }
+}
